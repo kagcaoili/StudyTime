9bf8aebbac44f692befc2c0f8d6d3aa3022799ca
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-02-13 13:31:36 -0800

    Updated view for calendarview and grabbed section name from json

--- routes/calendarView.js
+++ routes/calendarView.js
@@ -1,8 +1,9 @@
+var data = require('../data.json');
 
 /*
  * GET home page.
  */
 
 exports.view = function(req, res){
-  res.render('calendarView');
+  res.render('calendarView', data.assignments[0]);
 };
