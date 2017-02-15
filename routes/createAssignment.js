25cb8730c78fcde24b9b8d5d84075bf0a2da7f46
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-02-15 11:25:03 -0800

    Linked the createAccount page to app

--- routes/createAssignment.js
+++ routes/createAssignment.js
@@ -1,4 +1,4 @@
-
+var data = require('../data.json');
 
 
 /*
@@ -6,5 +6,23 @@
  */
 
 exports.view = function(req, res){
+
+  //how to get the length of json array in jquery?
+  //http://stackoverflow.com/questions/20811318/json-array-get-length-using-jquery-or-javascript
+  var array = [], len;
+  for (assignment in data.assignments) {
+  	array.push(assignment);
+  }
+  len = array.length;
+
+  var id = (data.assignments[len-1].id - -1);
+  var name = "Lab 3";
+  var due_date = "02/14/2017";
+  var class_name = "CSE 165";
+  var sections = [1, 2, 3];
+
+  data['assignments'].push({"id":id,"name":name,"due_date":due_date,"class":class_name, "sections":sections});
+  console.log(data);
+  
   res.render('createAssignment');
 };
