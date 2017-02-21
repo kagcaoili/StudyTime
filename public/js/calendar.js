a570519498609ec893f337f0ca40fb1553f4acc0
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-02-20 22:27:10 -0800

    Update calendar so it now follows current date. attempt to push data to json but change didn't work

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,27 +1,25 @@
 'use strict';
 
+var days_in_week_list = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+var months_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var days_in_month_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+var current_date = new Date();
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
 })
 
 function depressedButtonPrev(e) {
-	console.log("depress");
-	console.log($(this));
-	//$('.calnav-prev').addClass('depressed');
-	$(this).addClass('depressed');
-	console.log("the name is: " + $('.calnav-month').text());
 	$('.calnav-month').text(getNewName(-1));
+
 }
 
 function depressedButtonNext(e) {
-	console.log("depress");
-	console.log($(this));
-	//$('.calnav-prev').addClass('depressed');
-	$(this).addClass('depressed');
-	console.log("the name is: " + $('.calnav-month').text());
 	$('.calnav-month').text(getNewName(1));
 }
+
 
 
 /*
@@ -32,7 +30,45 @@ function initializePage() {
 	$(".calnav-prev").click(depressedButtonPrev);
 	$(".calnav-next").click(depressedButtonNext);
 	setClassColors();
+
+	makeCalendar(current_date.getMonth(), current_date.getFullYear());
 }
+
+
+
+function makeCalendar(month, year) {
+
+	var current_month = month;
+	var current_year = year;
+
+	var firstDay = new Date(current_year, current_month, 1);
+	var startingDay = firstDay.getDay();
+
+	var monthLength = days_in_month_list[current_month];
+
+	//Handle leap years
+	if (current_month == 1) {
+		if ((current_year % 4 == 0 && current_year % 100 != 0) || current_year % 400 == 0) {
+			monthLength = 29;
+		}
+	}
+
+	var monthName = months_list[current_month];
+	$('.calnav-month').text(monthName);
+	$('.calnav-year').text(current_year);
+
+
+}
+
+
+
+
+
+
+
+
+
+
 
 /*
  * Function that sets the colors of the events in the eventlist to
@@ -62,116 +98,88 @@ function inttoRGB(i) {
 }
 
 function getIndex() {
-	console.log("Getting Index for " + $('.calnav-month').text());
 	if ($('.calnav-month').text().trim() == "January") {
-		console.log("It is January");
 		return 0;
 	}
 	else if ($('.calnav-month').text().trim() == "February") {
-		console.log("It is February");
 		return 1;
 	}
-	else if ($('.calnav-month').text() === "March") {
-		console.log("It is March");
+	else if ($('.calnav-month').text().trim() === "March") {
 		return 2;
 	}
-	else if ($('.calnav-month').text() === "April") {
-		console.log("It is April");
+	else if ($('.calnav-month').text().trim() === "April") {
 		return 3;
 	}
-	else if ($('.calnav-month').text() === "May") {
-		console.log("It is May");
+	else if ($('.calnav-month').text().trim() === "May") {
 		return 4;
 	}
-	else if ($('.calnav-month').text() === "June") {
-		console.log("It is June");
+	else if ($('.calnav-month').text().trim() === "June") {
 		return 5;
 	}
-	else if ($('.calnav-month').text() === "July") {
-		console.log("It is July");
+	else if ($('.calnav-month').text().trim() === "July") {
 		return 6;
 	}
-	else if ($('.calnav-month').text() === "August") {
-		console.log("It is August");
+	else if ($('.calnav-month').text().trim() === "August") {
 		return 7;
 	}
-	else if ($('.calnav-month').text() === "September") {
-		console.log("It is September");
+	else if ($('.calnav-month').text().trim() === "September") {
 		return 8;
 	}
-	else if ($('.calnav-month').text() === "October") {
-		console.log("It is October");
+	else if ($('.calnav-month').text().trim() === "October") {
 		return 9;
 	}
-	else if ($('.calnav-month').text() === "November") {
-		console.log("It is November");
+	else if ($('.calnav-month').text().trim() === "November") {
 		return 10;
 	}
-	else if ($('.calnav-month').text() === "December") {
-		console.log("It is December");
+	else if ($('.calnav-month').text().trim() === "December") {
 		return 11;
 	}
 }
 
 function getName(index) {
 	var year = $('.calnav-year').text();
-	console.log("the year is: " + year);
 	if (index === -1) {
-		console.log("It is December");
 		year = year - 1;
 		$('.calnav-year').text(year);
 		return "December";
 	}
 	else if (index === 0) {
-		console.log("It is January");
 		return "January";
 	}
 	else if (index === 1) {
-		console.log("It is February");
 		return "February";
 	}
 	else if (index === 2) {
-		console.log("It is March");
 		return "March";
 	}
 	else if (index === 3) {
-		console.log("It is April");
 		return "April";
 	}
 	else if (index === 4) {
-		console.log("It is May");
 		return "May";
 	}
 	else if (index === 5) {
-		console.log("It is June");
 		return "June";
 	}
 	else if (index === 6) {
-		console.log("It is July");
 		return "July";
 	}
 	else if (index === 7) {
-		console.log("It is August");
 		return "August";
 	}
 	else if (index === 8) {
-		console.log("It is September");
 		return "September";
 	}
 	else if (index === 9) {
-		console.log("It is October");
 		return "October";
 	}
 	else if (index === 10) {
-		console.log("It is November");
 		return "November";
 	}
 	else if (index === 11) {
-		console.log("It is December");
 		return "December";
 	}
 	else if (index === 12) {
-		console.log("It is January");
 		year = +year+1; //act like an integer
 		$('.calnav-year').text(year);
 		return "January";
@@ -180,17 +188,9 @@ function getName(index) {
 
 function getNewName(index) {
 
-	console.log("Receiving change: " + index);
-
 	var currIndex = getIndex();
-
-	console.log("Current Month Index is: " + currIndex);
 	var newIndex = currIndex + index;
-
-	console.log("New Month Index is: " + newIndex);
 	var theNewName = getName(newIndex);
-
-	console.log("New Name Month is: " + theNewName);
 	return theNewName;
 
 }
--- routes/listview.js
+++ routes/listview.js
@@ -18,16 +18,32 @@ exports.view = function(req, res){
   //res.render('listview', data.assignments[idex]);
   //res.render('listview', req.body);
 
+  var id = req.body.id;
+  var name = req.body.name;
+  var due_date = req.body.due_date;
+  var class_name = req.body.class;
+  var sections = req.body.sections;
+
+  var newAssignment = {
+    "id": id,
+    "name": name,
+    "due_date": due_date,
+    "class": class_name,
+    "sections": sections,
+  }
+
+
   console.log("BEFORE: " + data.assignments[0].name);
-  data['assignments'].push({"id":req.body.id,"name":req.body.name,"due_date":req.body.due_date, "class":req.body.class, "sections":req.body.sections});
+  //data['assignments'].push({"id":req.body.id,"name":req.body.name,"due_date":req.body.due_date, "class":req.body.class, "sections":req.body.sections});
+  data.assignments.push(newAssignment);
   console.log("AFTER: " + data.assignments[2].name);
 
   res.render('listview', {
-    'id': req.body.id,
-    'name': req.body.name,
-    "due_date":req.body.due_date,
-    "class":req.body.class,
-    'section_name': req.body.sections
+    'id': id,
+    'name': name,
+    "due_date": due_date,
+    "class": class_name,
+    'section_name': sections
   });
 };
 
